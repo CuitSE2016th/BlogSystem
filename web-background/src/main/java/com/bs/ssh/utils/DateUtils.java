bc007109cd60741b190f09cf60a06d00afd9e92e
kangnima <41615592+kangnima@users.noreply.github.com>
2018-11-10 22:00:22 +0800

    优化 提高可读性

--- web-background/src/main/java/com/bs/ssh/utils/DateUtils.java
+++ web-background/src/main/java/com/bs/ssh/utils/DateUtils.java
@@ -54,7 +54,9 @@ public class DateUtils {
 
     public static boolean isSameDay(Calendar cal1, Calendar cal2) {
         if(cal1 != null && cal2 != null) {
-            return cal1.get(0) == cal2.get(0) && cal1.get(1) == cal2.get(1) && cal1.get(6) == cal2.get(6);
+            return cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA)
+                    && cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
+                    && cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
         } else {
             throw new IllegalArgumentException("The date must not be null");
         }
