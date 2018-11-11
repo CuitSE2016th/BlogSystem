4e077a9d22628f9ccedb83e2c84dc874323bd0e2
Egan <1154610080@qq.com>
2018-11-11 14:20:49 +0800

    添加HTTP响应体

---
+++ web-background/src/main/java/com/bs/ssh/beans/Result.java
@@ -0,0 +1,37 @@
+package com.bs.ssh.beans;
+
+/**
+ * HTTP响应体
+ *
+ * @author Egan
+ * @date 2018/11/11 14:18
+ **/
+public class Result<T> {
+    private int code;
+    private String message;
+    private T Data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return Data;
+    }
+
+    public void setData(T data) {
+        Data = data;
+    }
+}
