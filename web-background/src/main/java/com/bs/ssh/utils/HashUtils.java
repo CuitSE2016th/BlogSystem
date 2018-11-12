e1e836e6789bf7199654b32c7a1fbb1d13ff3919
Egan <1154610080@qq.com>
2018-11-12 23:33:02 +0800

    优化关注逻辑，要求关注者主动关注被关注者，不能相反

--- web-background/src/main/java/com/bs/ssh/utils/HashUtils.java
+++ web-background/src/main/java/com/bs/ssh/utils/HashUtils.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 public class HashUtils {
 
     public static String getSalt() {
-        return hashBySha1(Long.toString(System.currentTimeMillis()));
+        return hashBySha256(Long.toString(System.currentTimeMillis()));
     }
 
     public static String getToken() {
@@ -56,8 +56,8 @@ public class HashUtils {
      * @param input 加密原文
      * @return 散列后的 16 进制字符串
      */
-    public static String hashBySha1(String input) {
-        return hashBySha1(input.getBytes());
+    public static String hashBySha256(String input) {
+        return hashBySha256(input.getBytes());
     }
 
     /**
@@ -66,9 +66,9 @@ public class HashUtils {
      * @param bytes byte 数组
      * @return 散列后的 16 进制字符串
      */
-    public static String hashBySha1(byte[] bytes) {
+    public static String hashBySha256(byte[] bytes) {
         try {
-            MessageDigest digest = MessageDigest.getInstance("SHA1");
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
             digest.update(bytes);
             return bytesToHex(digest.digest());
         } catch (NoSuchAlgorithmException e) {
