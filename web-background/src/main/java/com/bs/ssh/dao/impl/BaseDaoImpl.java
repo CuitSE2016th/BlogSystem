a46564016b5b36bd47dd22de4e2117e95f1f3ae2
Egan <1154610080@qq.com>
2018-11-12 18:23:27 +0800

    完善Dao的更新数据功能

--- web-background/src/main/java/com/bs/ssh/dao/impl/BaseDaoImpl.java
+++ web-background/src/main/java/com/bs/ssh/dao/impl/BaseDaoImpl.java
@@ -10,6 +10,7 @@ import java.util.Iterator;
 import java.util.List;
 
 @Repository
+@Transactional
 public class BaseDaoImpl<T> implements BaseDao<T> {
 
     private HibernateTemplate template;
@@ -20,14 +21,26 @@ public class BaseDaoImpl<T> implements BaseDao<T> {
     }
 
     @Override
-    public <S extends T> S save(S entity) {
+    public <S extends T> S insert(S entity) {
         template.save(entity);
         return entity;
     }
 
     @Override
-    public <S extends T> Iterable<S> save(Iterable<S> entities) {
+    public <S extends T> Iterable<S> insert(Iterable<S> entities) {
         template.save(entities);
+        return entities;
+    }
+
+    @Override
+    public <S extends T> S update(S entity) {
+        template.update(entity);
+        return entity;
+    }
+
+    @Override
+    public <S extends T> Iterable<S> update(Iterable<S> entities) {
+        template.update(entities);
         return entities;
     }
 
