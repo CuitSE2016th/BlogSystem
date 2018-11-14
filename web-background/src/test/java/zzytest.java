164de38487922da0c9891d1c200734c8a4e16237
zzy <18382418120@163.com>
2018-11-14 20:40:02 +0800

    user regist is base ok ,but i need edit it, and make is pretty;

---
+++ web-background/src/test/java/zzytest.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Create By ZZY on 2018/11/14
+ */
+public class zzytest {
+}
