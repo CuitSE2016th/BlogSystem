36ecc5ca882af49cc4be398950445e183599b664
zzy <18382418120@163.com>
2018-11-15 16:18:25 +0800

    对邮箱进行大小写验证

--- web-background/src/test/java/zzytest.java
+++ web-background/src/test/java/zzytest.java
@@ -1,3 +1,4 @@
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -6,4 +7,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Create By ZZY on 2018/11/14
  */
 public class zzytest {
+
+    @Test
+    public void testsplit(){
+
+        String email = "1007610491@qq.com";
+        String[] split = email.split("@");
+        for (int i = 0; i < split.length; i++) {
+            System.out.println(split[i]);
+        }
+
+
+
+    }
+
 }
