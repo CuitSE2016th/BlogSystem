539ca2b509615736384387c4d7b45d00e05d2192
zzy <18382418120@163.com>
2018-11-23 19:32:16 +0800

    useradmin is ok

--- web-background/src/main/java/com/bs/ssh/service/admin/UserAdminService.java
+++ web-background/src/main/java/com/bs/ssh/service/admin/UserAdminService.java
@@ -1,6 +1,7 @@
 package com.bs.ssh.service.admin;
 
 import com.bs.ssh.beans.PageBean;
+import com.bs.ssh.beans.User;
 
 /**
  * Create By ZZY on 2018/11/22
@@ -9,4 +10,6 @@ public interface UserAdminService {
     PageBean getAllUserToPageBean(int pn, int pageSize);
 
     int deleteUserByUserID(String userID);
+
+    User getUserByUserID(String identity);
 }
