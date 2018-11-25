2a3821b40c3be3a2ddbbb8294ef9715eb7a33bb7
lfork <986204478@qq.com>
2018-11-25 22:01:38 +0800

    Finished  the UI of My Fragment

--- mobile-android/app/src/main/java/com/lfork/blogsystem/data/user/UserDataRepository.kt
+++ mobile-android/app/src/main/java/com/lfork/blogsystem/data/user/UserDataRepository.kt
@@ -1,7 +1,9 @@
 package com.lfork.blogsystem.data.user
 
+import com.lfork.blogsystem.BlogApplication
 import com.lfork.blogsystem.data.common.DataCallback
 import com.lfork.blogsystem.data.user.remote.UserRemoteDataSource
+import com.lfork.blogsystem.utils.StringValidation
 
 /**
  *
@@ -10,14 +12,34 @@ import com.lfork.blogsystem.data.user.remote.UserRemoteDataSource
 object UserDataRepository : UserDataSource {
     private val remoteDataSource = UserRemoteDataSource()
 
+    var isSignIn = false
+
     var userCache: User? = null
 
     var followings: List<User>? = null
 
     var followers: List<User>? = null
 
+    var userCacheIsDirty = true;
+
     override fun login(account: String, password: String, callback: DataCallback<String>) {
-        remoteDataSource.login(account, password, callback)
+        remoteDataSource.login(account, password, object : DataCallback<String> {
+            override fun succeed(data: String) {
+                BlogApplication.token = data
+                userCache = User()
+                if(StringValidation.isEmailValid(account)){
+                    userCache!!.email = account
+                } else {
+                    userCache!!.phone = account
+                }
+
+                callback.succeed(data)
+            }
+
+            override fun failed(code: Int, log: String) {
+                callback.failed(code, log)
+            }
+        })
     }
 
     override fun register(account: String, password: String, verifyCode: String, callback: DataCallback<String>) {
@@ -28,8 +50,23 @@ object UserDataRepository : UserDataSource {
         remoteDataSource.getVerificationCode(account, callback)
     }
 
-    fun getUserInfo(callback: DataCallback<List<User>>){
-        remoteDataSource.getUserInfo(callback)
+    fun getCurrentUserInfo(callback: DataCallback<User>) {
+
+        if (!userCacheIsDirty) {
+            callback.succeed(userCache!!)
+            return
+        }
+
+
+        if (userCache != null) {
+            callback.succeed(userCache!!)
+            userCacheIsDirty = false
+        } else {
+            callback.failed(-1, "用户未登录")
+        }
+
+
+//        remoteDataSource.getCurrentUserInfo(callback)
     }
 
 
