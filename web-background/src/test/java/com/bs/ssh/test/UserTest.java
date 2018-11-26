d0a7f625657cfddf2c7af378505bb41698259a45
Egan <1154610080@qq.com>
2018-11-26 23:00:05 +0800

    将所有测试移到基本包下

--- web-background/src/test/java/com/bs/ssh/test/UserTest.java
+++ web-background/src/test/java/com/bs/ssh/test/UserTest.java
@@ -1,20 +1,17 @@
-import com.bs.ssh.beans.Article;
-import com.bs.ssh.beans.Permission;
-import com.bs.ssh.beans.Role;
-import com.bs.ssh.beans.User;
+package com.bs.ssh.test;
+
+import com.bs.ssh.beans.*;
 import com.bs.ssh.dao.BaseDao;
 import com.bs.ssh.dao.PermissionDao;
 import com.bs.ssh.dao.RoleDao;
 import com.bs.ssh.dao.UserDao;
 import com.bs.ssh.utils.HashUtils;
 import com.bs.ssh.utils.IDUtils;
-import com.bs.ssh.utils.JsonUtil;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -54,24 +50,32 @@ public class UserTest {
         role.setName("user");
         role.setCreateTime(System.currentTimeMillis());
 
-        permission.setName("query-blog");
-        permission.setCreateTime(System.currentTimeMillis());
-        permissions.add(permission);
-        role.setPermissions(permissions);
+//        permission.setName("query-blog");
+//        permission.setCreateTime(System.currentTimeMillis());
+//        permissions.add(permission);
+//        role.setPermissions(permissions);
 
         roleDao.insert(role);
     }
 
-    //        @Test
+    @Resource
+    BaseDao<Follow> followDao;
+    @Test
+    @Transactional
     public void follow() {
-        List<User> users = new LinkedList<>();
-//        userDao.findAll("from User").forEach(users::add);
+//        List<User> users = new LinkedList<>();
+//        PageRequest request = new PageRequest();
+//        request.setPageNumber(1);
+//        request.setPageSize(2);
+//        users.addAll(userDao.findAll(request, "from User"));
 //
-//        User user3 = users.get(1);
-//        User user2 = users.get(2);
-//        List<User> user3Following = new LinkedList<>();
-//        user3Following.add(user2);
-//        user3.setFollowers(user3Following);
+//        User user1 = users.get(0);
+//        User user2 = users.get(1);
+        Follow follow = new Follow();
+        follow.setFollowerId("17826287827");
+        follow.setFollowingId("35268860677");
+        follow.setCreateTime(System.currentTimeMillis());
+        followDao.insert(follow);
 //
 //        userDao.update(user3);
 
@@ -79,18 +83,25 @@ public class UserTest {
 
     @Resource
     private BaseDao<Article> articleBaseDao;
-    //        @Test
+//    @Test
+    @Transactional
+    @Rollback(false)
     public void article(){
         User user = userDao.findByIdentity("13881705154");
         Article article = new Article();
-        article.setAuthor(user);
+        article.setAuthorId(user.getId());
         article.setContent("hello, there is a new article.");
         article.setStatus(0);
         article.setCreateTime(System.currentTimeMillis());
-        articleBaseDao.insert(article);
+        try {
+
+            articleBaseDao.insert(article);
+        }catch (Exception e){
+            logger.fatal(e.getCause());
+        }
     }
 
-    @Test
+//    @Test
     public void initUserAndToken() {
         User user1 = new User();
         user1.setId(IDUtils.UserID());
@@ -99,9 +110,7 @@ public class UserTest {
         user1.setSalt(HashUtils.getSalt());
         user1.setPassword(HashUtils.hashBySha256("123456" + user1.getSalt()));
         user1.setSex("M");
-        Role role = new Role();
-        role.setId(1);
-        user1.setRole(role);
+        user1.setRoleId(1);
 
         user1.setLastLoginTime(System.currentTimeMillis());
         user1.setCreateTime(System.currentTimeMillis());
@@ -114,20 +123,20 @@ public class UserTest {
         userDao.insert(user1);
     }
 
-    //    @Test
+    @Resource BaseDao<Star> starBaseDao;
+    @Resource BaseDao<Like> likeBaseDao;
+//        @Test
+//        @Transactional
     public void starAndLike(){
         User user = userDao.findByIdentity("13881705154");
-        System.out.println("uid:" + user.getId());
-        user.setNickname("eganchen");
 
         Article article = articleBaseDao.findOne("from Article");
         System.out.println(article.getContent());
-        List<Article> articles = new ArrayList<>();
-        articles.add(article);
-        user.setStarArticles(articles);
-        user.setLikeArticles(articles);
-        System.out.println(user.getLikeArticles().get(0).getContent());
-        userDao.update(user);
+
+        Star star = new Star();
+        star.setArticleId(article.getId());
+        star.setUserId(user.getId());
+        starBaseDao.insert(star);
     }
 
     @Resource
@@ -137,7 +146,7 @@ public class UserTest {
     public void entity(){
         User user = userBaseDao.findOne("from User where sex=?", "null");
 //        logger.fatal(user.getRole().getName());
-        logger.fatal(JsonUtil.toJsonExposed(user));
+//        logger.fatal(JsonUtils.toJsonExposed(user));
     }
 
     //    @Test
