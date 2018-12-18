c66854355da08dcddbf1eccbdfe15db09719adda
Egan <1154610080@qq.com>
2018-12-19 00:13:32 +0800

    初步完成文件上传

---
+++ web-background/src/main/java/com/bs/ssh/service/user/impl/UserFileServiceImpl.java
@@ -0,0 +1,64 @@
+package com.bs.ssh.service.user.impl;
+
+import com.bs.ssh.dao.BaseDao;
+import com.bs.ssh.entity.Image;
+import com.bs.ssh.exception.NoSuchEntityException;
+import com.bs.ssh.service.user.UserFileService;
+import com.bs.ssh.utils.IDUtils;
+import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+
+@Transactional
+@Service
+public class UserFileServiceImpl implements UserFileService {
+
+    private static String contextPath = "E:\\";
+
+    @Resource
+    private BaseDao<Image> imageBaseDao;
+
+    @Override
+    public String[] uploadPicture(String uid, File[] files, String[] filenames) throws IOException {
+
+        String[] paths = new String[files.length];
+
+        for(int i=0; i<files.length; i++){
+            int pos = filenames[i].lastIndexOf('.');
+
+            String imageId = IDUtils.UserID();
+            String suffix = pos == -1 ? "jpg" : filenames[i].substring(pos+1);
+
+            String path = contextPath + uid + "\\" +
+                    imageId + "." + suffix;
+
+            File target = new File(path);
+            FileUtils.copyFile(files[i], target);
+            paths[i] = path;
+
+            Image image = new Image();
+            image.setId(imageId);
+            image.setUid(uid);
+            image.setUrl(path);
+            image.setCreateTime(System.currentTimeMillis());
+
+            imageBaseDao.insert(image);
+        }
+        return paths;
+    }
+
+    @Override
+    public void referencePicture(String uid, String[] iid, Integer aid) {
+        for (String id : iid){
+            Image image = imageBaseDao.findOne("from Image where id=? and uid = ?", uid, id);
+            if(image == null)
+                throw new NoSuchEntityException("图片不存在或您没有操作权限");
+            image.setAid(aid);
+            imageBaseDao.update(image);
+        }
+    }
+}
--- web-background/src/main/webapp/index.jsp
+++ web-background/src/main/webapp/index.jsp
@@ -1,22 +1,42 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--<%@ page contentType="text/html;charset=UTF-8" language="java" %>--%>
+<%--<html>--%>
+<%--<body>--%>
+<%--<h2>Hello World!</h2>--%>
+<%--<form action="/blog/register" method="post">--%>
+    <%--邮箱：<input type="text" name="emailOrPhone"><br>--%>
+    <%--验证码：<input type="text" name="emailOrPhoneCode"><a name="code" href="/blog/code?emailOrPhone=1007610491@qq.com">获取邮箱验证码</a><br>--%>
+    <%--密码：<input type="password" name="password"><br>--%>
+    <%--<input type="submit">--%>
+<%--</form>--%>
+
+
+
+<%--</body>--%>
+<%--<script language="JavaScript" type="text/javascript" src="static/jquery-3.2.1.min.js">--%>
+
+    <%--window.onload = function () {--%>
+        <%--alert("test");--%>
+    <%--}--%>
+
+<%--</script>--%>
+<%--</html>--%>
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<%--pageEncoding="UTF-8"--%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>upload</title>
+</head>
 <body>
-<h2>Hello World!</h2>
-<form action="/blog/register" method="post">
-    邮箱：<input type="text" name="emailOrPhone"><br>
-    验证码：<input type="text" name="emailOrPhoneCode"><a name="code" href="/blog/code?emailOrPhone=1007610491@qq.com">获取邮箱验证码</a><br>
-    密码：<input type="password" name="password"><br>
-    <input type="submit">
-</form>
-
-
-
+<fieldset>
+    <legend>上传文件</legend>
+    <form action="${pageContext.request.contextPath}/user/article/upload" enctype="multipart/form-data" method="post">
+        <input type="file" name="image"/>
+        <input type="file" name="image"/>
+        <input type="submit" value="上传"/>
+    </form>
+</fieldset>
 </body>
-<script language="JavaScript" type="text/javascript" src="static/jquery-3.2.1.min.js">
-
-    window.onload = function () {
-        alert("test");
-    }
-
-</script>
 </html>
