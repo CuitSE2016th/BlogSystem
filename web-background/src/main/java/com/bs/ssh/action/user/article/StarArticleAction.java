12b85c8084490dd64f2b73e4812b1403565a23b8
Egan <1154610080@qq.com>
2018-12-19 17:11:50 +0800

    修复无法从前端获取实体ID的BUG

--- web-background/src/main/java/com/bs/ssh/action/user/article/StarArticleAction.java
+++ web-background/src/main/java/com/bs/ssh/action/user/article/StarArticleAction.java
@@ -3,16 +3,28 @@ package com.bs.ssh.action.user.article;
 
 import com.bs.ssh.bean.JsonBody;
 import com.opensymphony.xwork2.interceptor.annotations.InputConfig;
+import com.opensymphony.xwork2.validator.annotations.RequiredFieldValidator;
 import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
 import com.opensymphony.xwork2.validator.annotations.Validations;
+import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 
 @Namespace("/user/article")
 public class StarArticleAction extends AbstractArticleAction {
 
 
+    private Integer aid;
+    public Integer getAid() {
+        return aid;
+    }
+
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
+
+    @Action("/star")
     @Validations(
-            requiredStrings = {@RequiredStringValidator(fieldName = "aid", message = "文章ID不能为空")}
+            requiredFields = {@RequiredFieldValidator(fieldName = "aid", message = "文章ID不能为空")}
     )
     @InputConfig(methodName = "verify")
     @Override
--- web-background/src/main/java/com/bs/ssh/action/user/comment/DeleteCommentAction.java
+++ web-background/src/main/java/com/bs/ssh/action/user/comment/DeleteCommentAction.java
@@ -2,18 +2,27 @@ package com.bs.ssh.action.user.comment;
 
 import com.bs.ssh.bean.JsonBody;
 import com.opensymphony.xwork2.interceptor.annotations.InputConfig;
+import com.opensymphony.xwork2.validator.annotations.RequiredFieldValidator;
 import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
 import com.opensymphony.xwork2.validator.annotations.Validations;
 import org.apache.struts2.convention.annotation.Action;
 
 public class DeleteCommentAction extends AbstractCommentAction {
 
-    private int cid;
+    private Integer cid;
+    private Integer aid;
+    public Integer getAid() {
+        return aid;
+    }
+
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
 
     @Action("/user/comment/delete")
     @Validations(
-            requiredStrings = {
-                    @RequiredStringValidator(fieldName = "cid", message = "评论ID不能不为空")
+            requiredFields = {
+                    @RequiredFieldValidator(fieldName = "cid", message = "评论ID不能不为空")
             }
     )
     @InputConfig(methodName = "verify")
@@ -30,11 +39,11 @@ public class DeleteCommentAction extends AbstractCommentAction {
         return JSON;
     }
 
-    public int getCid() {
+    public Integer getCid() {
         return cid;
     }
 
-    public void setCid(int cid) {
+    public void setCid(Integer cid) {
         this.cid = cid;
     }
 }
