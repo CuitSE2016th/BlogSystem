8e8a2cd21e2b1d431d5d39cc646aa2b736510125
lfork <986204478@qq.com>
2018-12-19 10:01:37 +0800

    Added basic rich editor model

--- mobile-android/app/src/main/java/com/lfork/blogsystem/main/home/HomeFragment.kt
+++ mobile-android/app/src/main/java/com/lfork/blogsystem/main/home/HomeFragment.kt
@@ -8,17 +8,28 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.lfork.blogsystem.R
+import com.lfork.blogsystem.articleedit.ArticleEditorActivity
+import com.lfork.blogsystem.utils.startActivity
+import kotlinx.android.synthetic.main.main_home_frag.view.*
 
 class HomeFragment : Fragment() {
 
 
     private lateinit var viewModel: HomeViewModel
 
+    private var root:View?=null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_home_frag, container, false)
+        if (root == null){
+            root = inflater.inflate(R.layout.main_home_frag, container, false)
+            root!!.btn_edit.setOnClickListener { context?.startActivity<ArticleEditorActivity>() }
+        }
+
+        return root
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
