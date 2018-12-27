83f5383b9d70af8c705fae836e49474f409ee81a
Egan <1154610080@qq.com>
2018-12-28 02:41:53 +0800

    添加取消点赞和收藏接口

---
+++ web-background/src/main/java/com/bs/ssh/action/user/article/CancelLikeAction.java
@@ -0,0 +1,41 @@
+package com.bs.ssh.action.user.article;
+
+import com.bs.ssh.bean.JsonBody;
+import com.opensymphony.xwork2.interceptor.annotations.InputConfig;
+import com.opensymphony.xwork2.validator.annotations.RequiredFieldValidator;
+import com.opensymphony.xwork2.validator.annotations.Validations;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+
+@Namespace("/user/article")
+public class CancelLikeAction extends AbstractArticleAction {
+
+    private Integer aid;
+    public Integer getAid() {
+        return aid;
+    }
+
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
+
+    @Action("/cancellike")
+    @Validations(
+            requiredFields = {@RequiredFieldValidator(fieldName = "aid", message = "文章ID不能为空")}
+    )
+    @InputConfig(methodName = "verify")
+    @Override
+    public String execute() throws Exception {
+        try {
+            String uid = this.getUserId();
+            articleService.cancelLike(uid, aid);
+            result = JsonBody.success();
+        }catch (Exception e){
+            result = JsonBody.fail();
+            result.setData(e.getMessage());
+        }finally {
+            return JSON;
+        }
+    }
+
+}
